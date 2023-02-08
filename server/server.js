b3564ca182cfa241eb831220651eaeb36f71312b
Leon Forsythe <lvforsythe95@gmail.com>
2023-02-08 15:00:01 -0500

    built server and npm scripts"

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('/', (req, res){
-  
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 })
 
 app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
